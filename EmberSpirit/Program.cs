37da6b275b555b9d25e245cc4f62e8de6df904a0
tuedtvt <tuedtvt@gmail.com>
2015-10-18 01:25:44 +0700

    Update Program.cs

--- EmberSpirit/Program.cs
+++ EmberSpirit/Program.cs
@@ -17,11 +17,11 @@ namespace EmberSpirit
         private static bool toggle = true;
         private static Font txt;
         private static Font not;
-        private static Key KeyCombo = Key.D;
+        private static Key KeyCombo = Key.T;
         private static bool loaded;
         private static Hero me;
         private static Hero target;
-        private const Key ChaseKey = Key.D;
+        private const Key ChaseKey = Key.T;
         private static ParticleEffect rangeDisplay;
         static void Main(string[] args)
         {
